5b9df833fc1b231bc0268027b33f5f3a00bb1935
Ana <amlevina@yahoo.com>
2024-08-18 12:40:21 +0300

    Initial project

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,7 +16,7 @@ class BasePage:
     def open(self):
         self.driver.get(self.url)
 
-    def element_is_visible(self, locator, timeout=5):
+    def element_is_visible(self, locator, timeout=6):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
     def elements_are_visible(self, locator, timeout=5):
